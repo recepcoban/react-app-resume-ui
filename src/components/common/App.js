48f765c0d35296cd9ef0f4ccb9dfe3c6ec94ce0f
recepcoban <recep-coban@yandex.com>
2022-02-12 23:00:54 +0300

    create footer

--- src/components/common/App.js
+++ src/components/common/App.js
@@ -1,13 +1,31 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Navbar, NavbarBrand, Collapse, NavbarText } from "reactstrap";
 import Home from "../pages/Home";
 
 export default function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path="/" exact={true} component={Home} />
-      </Switch>
-    </Router>
+    <div>
+      <Router>
+        <Switch>
+          <Route path="/" exact={true} component={Home} />
+        </Switch>
+      </Router>
+
+      <Navbar color="dark" dark expand="md">
+        <Collapse className="container">
+          <NavbarBrand href="/">React App Resume UI</NavbarBrand>
+          <NavbarText>
+            Developed by{" "}
+            <a
+              href="https://www.linkedin.com/in/recepcoban"
+              className="text-info"
+            >
+              Recep Çoban
+            </a>
+          </NavbarText>
+        </Collapse>
+      </Navbar>
+    </div>
   );
 }
